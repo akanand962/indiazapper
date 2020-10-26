e74f663cc8068f1df13b0f46deb6fb57cbe7ce99
akanand962 <akanand962@gmail.com>
2020-10-27 01:31:03 +0530

    Updates

---
+++ precache-manifest.8633d7c7f64a2ef5c876c7aadf44915f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47599eaab51327679fd0b5bd85a76580",
+    "url": "/indiazapper/index.html"
+  },
+  {
+    "revision": "bc8cd5d98d027e58726b",
+    "url": "/indiazapper/static/css/2.c8bd06df.chunk.css"
+  },
+  {
+    "revision": "3f9a47b0a9a46fa633d7",
+    "url": "/indiazapper/static/css/main.6c92703d.chunk.css"
+  },
+  {
+    "revision": "bc8cd5d98d027e58726b",
+    "url": "/indiazapper/static/js/2.a0660129.chunk.js"
+  },
+  {
+    "revision": "22c95ed68071a711843b9981e2bca1af",
+    "url": "/indiazapper/static/js/2.a0660129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9a47b0a9a46fa633d7",
+    "url": "/indiazapper/static/js/main.ed9e68b1.chunk.js"
+  },
+  {
+    "revision": "5c0d031fe13e6a290836",
+    "url": "/indiazapper/static/js/runtime-main.e2b1dd37.js"
+  },
+  {
+    "revision": "878aafa93ea8a18a755976a8872a41d5",
+    "url": "/indiazapper/static/media/3.878aafa9.jpg"
+  },
+  {
+    "revision": "9a5db07d52e5ec32e3c7b5bfb4b16f7c",
+    "url": "/indiazapper/static/media/about-banner.9a5db07d.jpg"
+  },
+  {
+    "revision": "503da5e71200a7bc3dd1ec9c6c76ae0d",
+    "url": "/indiazapper/static/media/banner1.503da5e7.jpg"
+  },
+  {
+    "revision": "aa0eab6b654e0c547ea25e6ac0eed292",
+    "url": "/indiazapper/static/media/product1.aa0eab6b.jpg"
+  },
+  {
+    "revision": "fb2777db6f4f86de0a690b8e3dff1065",
+    "url": "/indiazapper/static/media/product2.fb2777db.png"
+  },
+  {
+    "revision": "cd27b27dbb07b64aa4e3561060cf2711",
+    "url": "/indiazapper/static/media/product3.cd27b27d.jpg"
+  },
+  {
+    "revision": "6d18868125e8d37b735df82534ae18e2",
+    "url": "/indiazapper/static/media/zapper img2.6d188681.jpg"
+  },
+  {
+    "revision": "1eb477695c34273a078639b3318a79a1",
+    "url": "/indiazapper/static/media/zapper img3.1eb47769.png"
+  },
+  {
+    "revision": "0d7ea94430ab1faf1030f065b483085c",
+    "url": "/indiazapper/static/media/zapper img4.0d7ea944.png"
+  },
+  {
+    "revision": "369f34c2e4e9cd29c3363b8921900bf1",
+    "url": "/indiazapper/static/media/zapper new.369f34c2.jpg"
+  }
+]);
