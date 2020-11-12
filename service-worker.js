5003409f9eda94850761f076d574d256f21ba89c
akanand962 <akanand962@gmail.com>
2020-11-13 03:30:35 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b7504828c731a12f7a5e4051ec37746.js"
+  "/precache-manifest.def50a9b071f9735baadc85ad6ff7535.js"
 );
 
 self.addEventListener('message', (event) => {
